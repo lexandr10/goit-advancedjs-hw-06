8057b831b374bff6e60e81a26a0c77875f3daf4b
lexandr10 <alexandrenko3006@gmail.com>
2023-12-06 20:06:30 -0500

    Ds number 6

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -3,5 +3,12 @@
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
 
-let union;
-let literal;
+let union: number | string;
+
+union = 10;
+union = 'Hello';
+
+
+type EnableOrDisable = "enable" | "disable";
+let literal: EnableOrDisable;
+literal = "enable";
--- src/baseTypes/7.ts
+++ src/baseTypes/7.ts
@@ -2,3 +2,24 @@
   Створіть функцію (isWeekend), яка приймає день тижня (з вашого enum)
   і повертає boolean значення, що вказує, чи це день робочий чи вихідний.
 */
+
+enum DayOfTheWeek {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday,
+}
+
+function isWeekend(day: string): boolean {
+  if (
+    day.toLocaleLowerCase() === "saturday" ||
+    day.toLocaleLowerCase() === "sunday"
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
